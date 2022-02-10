856e982acb9812f380a4367b2879259bbfc764cf
Erick Saavedra <esvdrah@gmail.com>
2022-02-10 15:00:31 -0500

    Fix components folder name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './commponents/Home';
-import SignIn from './commponents/SignIn';
-import SignUp from './commponents/SignUp';
+import Home from './components/Home';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
