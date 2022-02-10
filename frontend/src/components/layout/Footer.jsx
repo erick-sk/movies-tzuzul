34fe8258283efbcf993d0a80a9a882bdc91a9e0a
Erick Saavedra <esvdrah@gmail.com>
2022-02-10 16:18:29 -0500

    Add Footer component

---
+++ frontend/src/components/layout/Footer.jsx
@@ -0,0 +1,29 @@
+import { Box, Container, Spacer, Heading, Flex, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+  return (
+    <Box bg="black" pt={5} pb={5} color="white">
+      <Container maxW={['container.lg', null, null, 'container.xl']}>
+        <Flex>
+          <Heading fontWeight="normal" size="lg">
+            <Link to="/">
+              Movies
+              <Box as="span" fontWeight="bold">
+                Now
+              </Box>
+            </Link>
+          </Heading>
+
+          <Spacer />
+
+          <Box>
+            <Text>&copy; All rights reserved </Text>
+          </Box>
+        </Flex>
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
