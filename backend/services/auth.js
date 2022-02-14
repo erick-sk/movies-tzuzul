99fbda8ed23cc08d5f8b10eee03801f072ea23c4
Erick Saavedra <esvdrah@gmail.com>
2022-02-14 14:37:18 -0500

    Add hash to password

--- backend/services/auth.js
+++ backend/services/auth.js
@@ -1,21 +1,30 @@
 const Users = require('./users');
 const jwt = require('jsonwebtoken');
 const { jwt_secret } = require('../config');
+const bcrypt = require('bcrypt');
 
 class Auth {
   constructor() {
     this.users = new Users();
   }
 
+  async hashPassword(password) {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    return hash;
+  }
+
   async signin(email, password) {
     const user = await this.users.getByEmail(email);
 
-    if (user && user.password === password) {
+    const correctPassword = await bcrypt.compare(password, user.password);
+    if (user && correctPassword) {
       const data = {
         firstName: user.firstName,
         lastName: user.lastName,
         email: user.email,
-        role: user.role ? user.role : 1,
+        // role: user.role ? user.role : 1,
       };
 
       const token = jwt.sign(data, jwt_secret, { expiresIn: '1d' });
@@ -28,12 +37,14 @@ class Auth {
     if (await this.users.getByEmail(userData.email)) {
       return { success: false, message: 'The user is already registered!' };
     } else {
+      userData.role = 1;
+      userData.password = await this.hashPassword(userData.password);
       const user = await this.users.create(userData);
       const data = {
         firstName: user.firstName,
         lastName: user.lastName,
         email: user.email,
-        role: user.role ? user.role : 1,
+        role: 0,
       };
       const token = jwt.sign(data, jwt_secret, { expiresIn: '1d' });
       return { success: true, data, token };
