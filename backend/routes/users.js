73845e321b41387172a54f41c16dd88fb73051e3
Erick Saavedra <esvdrah@gmail.com>
2022-02-17 16:51:29 -0500

    Add create, read, update and delete reviews

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,29 +7,33 @@ function users(app) {
   const usersService = new Users();
   app.use('/api/users', router);
 
+  // get one user
   router.get('/:id', isAdmin, async (req, res) => {
     const { id } = req.params;
     const user = await usersService.get(id);
     return res.status(200).json(user);
   });
 
+  // get all user
   router.get('/', isAdmin, async (req, res) => {
-    console.log(req.user);
     const users = await usersService.getAll();
     return res.status(200).json(users);
   });
 
+  // create user
   router.post('/', isAdmin, async (req, res) => {
     const user = await usersService.create(req.body);
     return res.status(201).json(user);
   });
 
+  // update user
   router.put('/:id', isAdmin, async (req, res) => {
     const { id } = req.params;
     const user = await usersService.update(id, req.body);
     return res.status(200).json(user);
   });
 
+  // delete user
   router.delete('/:id', isAdmin, async (req, res) => {
     const { id } = req.params;
     const user = await usersService.delete(id);
